a4cb86e806f1c8a91651683febe35f7948939400
Milton Österberg <miltonwendel@outlook.com>
2024-01-29 12:42:41 +0100

    fix

--- Program.cs
+++ Program.cs
@@ -10,13 +10,47 @@ class Program
     public static void Main()
     {
         List<Hotel> hotels = HotelDatabase.GetHotels();
+        List<Room> rooms = HotelDatabase.GetRooms();
 
         foreach (Hotel h in hotels)
         {
             Console.WriteLine($"Name: {h.Name}, Rating: {h.Rating}");
         }
+        foreach (Room r in rooms)
+        {
+            Console.WriteLine($"RoomNr: {r.ID}\nPrice: {r.Price}\nStatus: {r.Status}\n");
+        }
     }
 }
+
+
+class HotelDatabase
+{
+    static string connectionString = "server=localhost;database=hotell;uid=root;pwd=;";
+    
+    static IDbConnection connection = new MySqlConnection(connectionString);
+    
+    
+    public static List<Hotel> GetHotels()
+    {
+        string sql = "SELECT Name, Rating " +  
+            "FROM hotels; ";
+
+        var hotels = connection.Query<Hotel>(sql).AsList();
+        
+        return hotels;
+    }
+    public static List<Room> GetRooms()
+    {
+        string sql = "SELECT ID, Price, Status " +
+            "FROM rooms; ";
+        var rooms = connection.Query<Room>(sql).AsList();
+
+        return rooms;
+    }
+}
+
+
 class Hotel
 {
     public string Name {get; set;}
@@ -31,21 +65,19 @@ class Hotel
         Rating = rating;
     }
 }
-
-class HotelDatabase
+class Room
 {
-    static string connectionString = "server=localhost;database=hotell;uid=root;pwd=;";
-    
-    static IDbConnection connection = new MySqlConnection(connectionString);
-    
-    
-    public static List<Hotel> GetHotels()
+    public int ID {get; set;}
+    public int Price {get; set;}
+    public bool Status {get; set;}
+    public Room()
     {
-        string sql = "SELECT Name, Rating " +  
-            "FROM hotels";
 
-        var hotels = connection.Query<Hotel>(sql).AsList();
-        
-        return hotels;
+    }
+    public Room(int id, int price, bool status)
+    {
+        ID = id;
+        Price = price;
+        Status = status;
     }
 }
